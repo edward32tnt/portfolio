e5898d4313e57f6a01e0d27d562e504bdf3ac26a
zhaochunyu <zhaochunyu@bilibili.com>
2023-03-13 16:12:10 +0800

    feat: add guess boook page

--- libs/apollo.ts
+++ libs/apollo.ts
@@ -4,6 +4,9 @@ import { setContext } from '@apollo/client/link/context'
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_HYGRAPH_URL,
 })
+const writeLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_WRITE_HYGRAPH_URL,
+})
 
 const authLink = setContext((_, { headers }) => {
   const token = process.env.NEXT_PUBLIC_AUTH_TOKEN
@@ -17,6 +20,13 @@ const authLink = setContext((_, { headers }) => {
 
 export const client = new ApolloClient({
   link: authLink.concat(httpLink),
+  cache: new InMemoryCache({
+
+  })
+})
+
+export const writeClient = new ApolloClient({
+  link: authLink.concat(writeLink),
   cache: new InMemoryCache({
 
   })
--- pages/guess-book/index.tsx
+++ pages/guess-book/index.tsx
@@ -1,3 +1,117 @@
-export default function GuessBook() {
-  return <span className="text-2xl"> guess book </span>;
+import { NextPage } from 'next';
+import client, { writeClient } from '../../libs/apollo';
+import {
+  createGuessbook,
+  getGuessbook,
+  publishGuessbook
+} from '../../libs/apolloQuerys';
+import Comment, { CommentProps } from '../../components/Comment';
+import { FormEventHandler, useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { useRouter } from 'next/router';
+import { create } from 'domain';
+interface Props {
+  comments: CommentProps[];
 }
+
+const GuessBook: NextPage<Props> = ({ comments }) => {
+  const [theData, setTheData] = useState(comments);
+  const [isPending, setPending] = useState(false);
+  const [formData, setFormData] = useState({
+    nickname: '',
+    content: ''
+  });
+  const handleSubmit: FormEventHandler = async (e) => {
+    setFormData({ nickname: '', content: '' });
+    setPending(true);
+    e.preventDefault();
+    const {
+      data: {
+        createGuessBook: { id }
+      }
+    } = await writeClient.mutate({
+      mutation: createGuessbook,
+      variables: {
+        ...formData
+      }
+    });
+    await writeClient.mutate({
+      mutation: publishGuessbook,
+      variables: {
+        id: id
+      }
+    });
+
+    const { data } = await client.query({
+      query: getGuessbook
+    });
+    setPending(false);
+    setTheData(data.guessBooks);
+
+    return false;
+  };
+  return (
+    <div className="p-4 flex flex-col gap-4">
+      <form
+        className="rounded p-4 bg-gray-50 flex flex-col gap-2"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="rounded p-2 focus-visible:outline-none focus-visible:ring focus-visible:ring-sky-200"
+          type="text"
+          name="nickName"
+          value={formData.nickname}
+          onChange={(e) =>
+            setFormData({
+              nickname: e.target.value,
+              content: formData.content
+            })
+          }
+          placeholder="NickName"
+        />
+        <p className="flex gap-4 justify-start">
+          <textarea
+            className="p-2 rounded w-full focus-visible:outline-none focus-visible:ring focus-visible:ring-sky-200"
+            name="content"
+            placeholder="Content"
+            value={formData.content}
+            onChange={(e) =>
+              setFormData({
+                nickname: formData.nickname,
+                content: e.target.value
+              })
+            }
+            cols={30}
+            rows={2}
+          ></textarea>
+          <input
+            className={classNames({
+              'font-bold': true,
+              'px-5': true,
+              rounded: true,
+              'bg-sky-300': !isPending,
+              'bg-gray-300': isPending,
+              'focus:ring-1': true
+            })}
+            disabled={isPending}
+            type="submit"
+            value={isPending ? 'Sending...' : 'SEND'}
+          />
+        </p>
+      </form>
+      {theData.map((x) => (
+        <Comment key={'comment-card-' + x.id} {...x} />
+      ))}
+    </div>
+  );
+};
+GuessBook.getInitialProps = async () => {
+  const { data } = await client.query({
+    query: getGuessbook
+  });
+  return {
+    comments: data.guessBooks
+  };
+};
+
+export default GuessBook;
